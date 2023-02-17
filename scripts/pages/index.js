8ccbd49015edf7da51f84c940048240d7b782fee
Hicham Moustaid <hmoustaid.lrn@gmail.com>
2023-02-17 17:28:23 +0100

    Récupérer les photographes à l'aide de fetch et les afficher au console

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,46 +1,45 @@
-    async function getPhotographers() {
-        // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
-        // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois récupéré
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
-    }
 
-    async function displayData(photographers) {
-        const photographersSection = document.querySelector(".photographer_section");
+async function getPhotographers() {
+		//Récuperer les infos du photographe. On récupère un objet représentant la réponse HTTP.
+		const response = await fetch('./data/photographers.json')
+		//Extraire les données JSNON de la réponse
+		const data = await response.json()
+        //Créer un tableau ou on va récuperer les photographes
+        const photographers = [];
+        //Parcourrir les données JSON qu'on a obtenues et insérer chaque photographe dans le tableau
+        data.photographers.forEach(photograph => {
+            // Création d'un objet photographe avec les propriétés pertinentes
+            const photographObj = {
+              name: photograph.name,
+              id: photograph.id,
+              city: photograph.city,
+              country: photograph.country,
+              tagLine: photograph.tagLine,
+              price: photograph.price,
+              portrait: photograph.portrait
+            };
+            // Ajout de l'objet photographe au tableau
+            photographers.push(photographObj);
+       })
+		return photographers
+}
 
-        photographers.forEach((photographer) => {
-            const photographerModel = photographerFactory(photographer);
-            const userCardDOM = photographerModel.getUserCardDOM();
-            photographersSection.appendChild(userCardDOM);
-        });
-    };
 
-    async function init() {
-        // Récupère les datas des photographes
-        const { photographers } = await getPhotographers();
-        displayData(photographers);
-    };
-    
-    init();
-    
+async function displayData(photographers) {
+    const photographersSection = document.querySelector(".photographer_section");
+
+    photographers.forEach((photographer) => {
+        const photographerModel = photographerFactory(photographer);
+        const userCardDOM = photographerModel.getUserCardDOM();
+        photographersSection.appendChild(userCardDOM);
+    });
+};
+
+async function init() {
+    // Récupère les datas des photographes
+    const photographers  = await getPhotographers();
+    console.log(photographers);
+};
+
+init();
+
