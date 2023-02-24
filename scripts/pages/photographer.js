9547dd77cf648af01934a63aad867fb140e7c1b5
Hicham Moustaid <hmoustaid.lrn@gmail.com>
2023-02-24 02:09:44 +0100

    The photographer's page header without css finished

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,37 @@
-//Mettre le code JavaScript lié à la page photographer.html
+const searchParams = new URLSearchParams(location.search)
+const photographerId = +searchParams.get('id')
+
+
+async function displayHeader(photographer) {
+    const photographerHeader = document.querySelector(".photograph-header")
+    const photographerInfo = document.createElement('div')
+    photographerInfo.classList.add('photograph-infos')
+    const { name, picture, getUserCardDOM} = photographerFactory(photographer)
+    const h1 = document.createElement('h1')
+    h1.textContent = name
+    const city = document.createElement('p')
+    console.log(city)
+    city.textContent = getUserCardDOM().querySelector("#city").textContent
+    const tag = document.createElement('p')
+    tag.textContent = getUserCardDOM().querySelector("#tag").textContent
+    photographerInfo.appendChild(h1)
+    photographerInfo.appendChild(city)
+    photographerInfo.appendChild(tag)
+    photographerHeader.appendChild(photographerInfo)
+    const image = document.createElement('img')
+    image.src = picture
+	image.alt = name
+    photographerHeader.appendChild(image)
+};
+
+async function init() {
+    //on attend une reponse de la requete fetch pour recuperer les infos du photographe
+	const response = await fetch('./data/photographers.json')
+	//on attend une reponse de la requete fetch pour recuperer les donnes en JSON
+	const data = await response.json()
+	//on affecte les infos du photographe a la variable photographer
+	const photographer = data.photographers.find((photograph) => photograph.id === photographerId)
+    displayHeader(photographer)
+};
+
+init();
