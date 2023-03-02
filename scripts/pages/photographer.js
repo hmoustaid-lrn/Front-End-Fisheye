fefaa0002de3df305963846055d4b2a5c1092a3a
Hicham Moustaid <hmoustaid.lrn@gmail.com>
2023-03-02 17:33:01 +0100

    display of sorting implementation modified

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -25,33 +25,6 @@ async function displayHeader(photographer) {
     photographerHeader.appendChild(image)
 };
 
-async function displaySorting() {
-    const main = document.querySelector("main")
-    const sorting = document.createElement("div")
-    sorting.id = "sort"
-    const label = document.createElement("label")
-    label.setAttribute("for", "sortingSelect")
-    label.textContent = "Trier par"
-    const select = document.createElement("select")
-    select.setAttribute("name", "sortingSelect")
-    select.setAttribute("id", "sortingSelect")
-    const option1 = document.createElement('option')
-    option1.setAttribute("value", "popularity")
-    option1.textContent = "Popularité"
-    select.appendChild(option1)
-    const option2 = document.createElement('option')
-    option2.setAttribute("value", "date")
-    option2.textContent = "Date"
-    select.appendChild(option2)
-    const option3 = document.createElement("option")
-    option3.setAttribute("value", "title")
-    option3.textContent = "Titre"
-    select.appendChild(option3)
-    sorting.appendChild(label);
-    sorting.appendChild(select);
-    main.appendChild(sorting);
-};
-
 async function init() {
     //on attend une reponse de la requete fetch pour recuperer les infos du photographe
     const response = await fetch('./data/photographers.json')
@@ -60,8 +33,6 @@ async function init() {
     //on affecte les infos du photographe a la variable photographer
     const photographer = data.photographers.find((photograph) => photograph.id === photographerId)
     displayHeader(photographer)
-    displaySorting()
-
 };
 
 init();
