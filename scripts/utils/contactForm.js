827355db56f778535a893f6d02462218e0c62b1d
Hicham Moustaid <hmoustaid.lrn@gmail.com>
2023-03-03 17:32:45 +0100

    console.log to display the user-inputted data
    This is to simulate a successful submission of the form data.

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -7,3 +7,15 @@ function closeModal() {
     const modal = document.getElementById("contact_modal");
     modal.style.display = "none";
 }
+
+function sendMessage(event) {
+	event.preventDefault()
+	for (const element of event.target.elements) {
+		if (element.tagName.toLowerCase() === 'input' || element.tagName.toLowerCase() === 'textarea') {
+			console.log(element.value)
+            //Vider les champs du modal avant de le fermer afin d'éviter que les champs du modal ne contiennent les anciennes informations saisies
+			element.value = ''
+		}
+	}
+	closeModal()
+}
