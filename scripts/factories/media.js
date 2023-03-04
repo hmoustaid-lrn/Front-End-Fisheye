d94e9b15b67b367b1d3bb35c80850815b4262998
Hicham Moustaid <hmoustaid.lrn@gmail.com>
2023-03-04 21:48:18 +0100

    Now a media cannot be liked more than once

--- scripts/factories/media.js
+++ scripts/factories/media.js
@@ -3,7 +3,7 @@ function mediaFactory(media) {
     const article = document.createElement('article')
     article.id = media.id
     const link = document.createElement('a')
-    link.href = "#"
+    link.href = "javascript:void(0);"
     const mediaElement = media.video ? document.createElement('video') : document.createElement('img')
     mediaElement.src = `./assets/photographers/Photographers media/${media.photographerName.split(" ")[0]}/${media.video ?? media.image}`
     mediaElement.alt = media.title
@@ -14,12 +14,12 @@ function mediaFactory(media) {
     const likesImage = document.createElement('p')
     titleImage.textContent = media.title
     likesImage.textContent = media.likes + ' ♥'
-    likesImage.addEventListener('click', function(event) {
-        event.preventDefault()
+    likesImage.addEventListener('click', function handleLikesClick(event) {
         likesImage.textContent = ++media.likes + ' ♥'
         const likesAndPrice = document.querySelector("#photograph-likes-price")
         const updatedNumberOfLikes = parseInt(likesAndPrice.children[0].textContent.split(" ")[0]) + 1
         likesAndPrice.children[0].textContent = updatedNumberOfLikes + ' ♥'
+        likesImage.removeEventListener('click', handleLikesClick);
     })
     article.appendChild(mediaElement)
     divCaption.appendChild(titleImage)
